ab22f7bbc8df087ed33d8666f52ec7de15ebca3c
Kenny Do <chinesedewey@gmail.com>
2016-10-29 11:59:57 -0700

    Add setup.py and other packaging files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+
+setup(
+    name='papika',
+    version='0.0.1',
+    description="Emit and receive Slack messages over Kafka",
+    url='https://github.com/kennydo/papika',
+    author='Kenny Do',
+    author_email='chinesedewey@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet',
+    ],
+    packages=find_packages(exclude=['tests']),
+    package_data={
+    },
+    include_package_Data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
